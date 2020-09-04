a2f6085aad38fce1671d123a718694d938e3361a
Fromus80 <48888188+Fromus80@users.noreply.github.com>
2020-09-04 07:37:43 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
 
+
 loadkeys ru
-sleep 10
 setfont cyr-sun16
-sleep 10
-pacman-key --refresh-keys
-sleep 10
+
 echo '2.3 Синхронизация системных часов'
 timedatectl set-ntp true
 
-echo 'Ваша разметка диска'
 sleep 10
+
+echo 'Ваша разметка диска'
 fdisk -l
 sleep 10
-echo '2.4 создание разделов'
-cfdisk /dev/sdd
+
+cfdisk /dev/sde
+
 echo '2.4.2 Форматирование дисков'
 
-mkfs.fat -F32 /dev/sdd1
-sleep 10
-mkfs.ext4  /dev/sdd2
-sleep 10
-mkfs.ext4  /dev/sdd3
+mkfs.fat -F32 /dev/sde1
+mkfs.ext4  /dev/sde2
+mkfs.ext4  /dev/sde3
 
 echo '2.4.3 Монтирование дисков'
-mount /dev/sdd2 /mnt
+mount /dev/sde2 /mnt
 mkdir /mnt/home
 mkdir -p /mnt/boot/efi
-mount /dev/sdd1 /mnt/boot/efi
-mount /dev/sdd3 /mnt/home
+mount /dev/sde1 /mnt/boot/efi
+mount /dev/sde3 /mnt/home
+
+sleep 10
 
 echo '3.1 Выбор зеркал для загрузки.'
 nano /etc/pacman.d/mirrorlist
-
-
+sleep 10
 echo '3.2 Установка основных пакетов'
 pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
 
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/Fromus80/arch/master/arch2.sh)"
+arch-chroot /mnt sh -c "$(curl -fsSL git.io/archuefi2.sh)"
