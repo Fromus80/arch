7d3317e831371d4177cf53099cd99f60a9a72e8e
Fromus80 <48888188+Fromus80@users.noreply.github.com>
2020-09-04 15:04:19 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -1,52 +1,5 @@
 #!/bin/bash
 
-
-loadkeys ru
-setfont cyr-sun16
-pacman -Sy
-
-echo '2.3 Синхронизация системных часов'
-timedatectl set-ntp true
-
-sleep 10
-echo "Добро пожаловать в установку ArchLinux"
-sleep 10
-echo 'Ваша разметка диска'
-fdisk -l
-sleep 10
-
-cfdisk /dev/sdd
-sleep 10
-echo '2.4.2 Форматирование дисков'
-sleep 10
-mkfs.fat -F32 /dev/sdd1
-sleep 10
-mkfs.ext4  /dev/sdd2
-sleep 10
-mkfs.ext4  /dev/sdd3
-sleep 10
-echo '2.4.3 Монтирование дисков'
-sleep 10
-mount /dev/sdd2 /mnt
-sleep 10
-mkdir -p /mnt/boot/efi
-sleep 10
-mount /dev/sdd1 /mnt/boot/efi
-sleep 10
-mkdir /mnt/home
-sleep 10
-fdisk -l
-sleep 10
-mount /dev/sdd3 /mnt/home
-
-sleep 10
-
-echo '3.1 Выбор зеркал для загрузки.'
-sleep 10
-rm -rf /etc/pacman.d/mirrorlist
-wget https://git.io/mirrorlist
-mv -f ~/mirrorlist /etc/pacman.d/mirrorlist
-sleep 10
 echo '3.2 Установка основных пакетов'
 sleep 10
 pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl wget
