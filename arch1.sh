60f28737637cf06414031f0989fc93df0bc45e52
Fromus80 <48888188+Fromus80@users.noreply.github.com>
2020-09-04 12:30:47 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -4,7 +4,7 @@
 loadkeys ru
 setfont cyr-sun16
 pacman -Sy
-pacman-key --refresh-keys 
+
 echo '2.3 Синхронизация системных часов'
 timedatectl set-ntp true
 
@@ -22,8 +22,9 @@ echo '2.4.2 Форматирование дисков'
 mkfs.fat -F32 /dev/sdd1
 mkfs.ext4  /dev/sdd2
 mkfs.ext4  /dev/sdd3
-
+sleep 10
 echo '2.4.3 Монтирование дисков'
+sleep 10
 mount /dev/sdd2 /mnt
 mkdir /mnt/home
 mkdir -p /mnt/boot/efi
@@ -33,12 +34,17 @@ mount /dev/sdd3 /mnt/home
 sleep 10
 
 echo '3.1 Выбор зеркал для загрузки.'
-nano /etc/pacman.d/mirrorlist
+sleep 10
+rm -rf /etc/pacman.d/mirrorlist
+wget https://git.io/mirrorlist
+mv -f ~/mirrorlist /etc/pacman.d/mirrorlist
 sleep 10
 echo '3.2 Установка основных пакетов'
+sleep 10
 pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl wget
-
+sleep 10
 echo '3.3 Настройка системы'
+sleep 10
 genfstab -pU /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/Fromus80/arch/master/arch2.sh)"
