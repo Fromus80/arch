24e026bdfee513fc10a1ee4eb751f3f7ce9f4eca
Fromus80 <48888188+Fromus80@users.noreply.github.com>
2020-09-12 07:37:21 +0300

    Create arch3.sh

---
+++ arch3.sh
@@ -0,0 +1,48 @@
+sleep 10
+pacman -S exfat-utils ntfs-3g
+sleep 10
+echo ' Ставим рограммы для работы с архивами'
+sleep 10
+pacman -S file-roller p7zip unace lrzip unzip unrar lha ark
+sleep 10
+echo ' Ставим подключение Android к ПК через USB? '
+sleep 10
+pacman -S gvfs-mtp
+sleep 10
+echo ' htop--диспетчер задач для linux  '
+sleep 10
+pacman -S htop xterm
+sleep 10
+echo ' gwenview - программа для просмотра изображений и PDF, калькулятор'
+sleep 10
+pacman -S gwenview okular kcalc
+sleep 10
+echo ' Ставим Steam   '
+pacman -S steam
+sleep 10
+echo 'Ставим neofetch screenfetch'
+sleep 10
+pacman -S screenfetch neofetch
+sleep 10
+echo 'Ставим VLC'
+sleep 10
+pacman -S vlc
+sleep 10
+echo 'Ставим gparted - программу для работы с разделами sdd/hdd'
+sleep 10
+pacman -S gparted
+sleep 10
+echo 'Ставим telegram - мессенджер'
+sleep 10
+pacman -S telegram-desktop
+sleep 10
+echo 'Ставим программы для создания скриншотов'
+sleep 10
+pacman -S spectacle flameshot
+sleep 10
+
+ 
+echo 'Ставим reflector'
+sleep 10
+pacman -S reflector
+sleep 10
