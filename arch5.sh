ee83233038c5e07b28f2c5e8b778059ad0ade82e
Fromus80 <48888188+Fromus80@users.noreply.github.com>
2020-09-24 00:15:06 +0300

    Update and rename arch4.sh to arch5.sh

---
+++ arch5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget git.io/yay-install.sh && sh yay-install.sh --noconfirm
+yay -S google-chrome --noconfirm
+sudo pacman -S zsh --noconfirm
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+exit
