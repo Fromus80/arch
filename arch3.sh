59ad5bfab165481174a700a36821f4a43e704237
Fromus80 <48888188+Fromus80@users.noreply.github.com>
2020-10-01 08:24:30 +0300

    Update arch3.sh

--- arch3.sh
+++ arch3.sh
@@ -3,6 +3,8 @@ wget git.io/yay-install.sh && sh yay-install.sh --noconfirm
 yay -S google-chrome --noconfirm
 sudo pacman -S zsh --noconfirm
 chsh -s /bin/zsh
+zsh
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
