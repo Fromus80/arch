cc215452580cd9918267928486474bf42ba1b1bc
Fromus80 <48888188+Fromus80@users.noreply.github.com>
2020-10-06 17:49:35 +0300

    Update arch3.sh

--- arch3.sh
+++ arch3.sh
@@ -9,8 +9,8 @@ echo 'Ставим Видео драйвер'
 timedatectl
 timedatectl set-local-rtc 1 --adjust-system-clock
 timedatectl
-pacman -S nvidia nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
 
+sudo pacman -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader nvidia --noconfirm
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
