0793d3e6222f223604ee9ac89adce7979728a91f
Fromus80 <48888188+Fromus80@users.noreply.github.com>
2020-10-18 18:46:46 +0300

    Update arch4.sh

--- arch4.sh
+++ arch4.sh
@@ -18,7 +18,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 yay -S conky-manager2 conky-cairo autojump --noconfirm
-yay -S lutris --noconfirm
+yay -S crossover --noconfirm
 wget git.io/conky.tar.gz
 tar -xzf conky.tar.gz -C ~/
 
