03b7ec3145439599662b610f74fa0d15e4353bbf
Fromus80 <48888188+Fromus80@users.noreply.github.com>
2020-10-26 11:25:02 +0300

    Update arch4.sh

--- arch4.sh
+++ arch4.sh
@@ -78,17 +78,7 @@ yay -S ttf-clear-sans --noconfirm
 #yay -S x-arc-shadow --noconfirm
 #yay -S papirus-maia-icon-theme-git --noconfirm
 #yay -S breeze-default-cursor-theme --noconfirm
-sudo pacman -S zsh --noconfirm
 
-zsh
-chsh -s /bin/zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 
 exit
