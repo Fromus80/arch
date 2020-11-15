5a75bc7f3df615e844491989b71fbbb81c219a4f
Fromus80 <48888188+Fromus80@users.noreply.github.com>
2020-11-15 07:26:41 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -2,19 +2,18 @@
 
 loadkeys ru
 setfont cyr-sun16
-sleep 10
 echo '2.3 Синхронизация системных часов'
-sleep 10
+
 timedatectl set-ntp true
-sleep 10
+
 
 echo 'Ваша разметка диска'
-sleep 10
+
 fdisk -l
 lsblk
-sleep 10
+
 echo '2.4.2 Форматирование дисков'
-sleep 10
+
 mkfs.fat -F32 /dev/sdd1
 mkfs.ext4  /dev/sdd2
 mkfs.ext4  /dev/sdd3
@@ -30,9 +29,9 @@ lsblk
 echo '3.2 Установка основных пакетов'
 sleep 10
 pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
-sleep 10
+
 echo '3.3 Настройка системы'
-sleep 10
+
 genfstab -pU /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/Fromus80/arch/master/arch2.sh)"
