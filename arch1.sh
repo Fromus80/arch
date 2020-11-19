24f566e3ebe4716c3004a62487bfaf4d50a62108
Fromus80 <48888188+Fromus80@users.noreply.github.com>
2020-11-19 10:37:27 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -28,7 +28,7 @@ mount /dev/sdd3 /mnt/home
 lsblk
 echo '3.2 Установка основных пакетов'
 sleep 10
-pacstrap /mnt base base-devel linux-lts linux-firmware nano dhcpcd netctl
+pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
 
 echo '3.3 Настройка системы'
 
