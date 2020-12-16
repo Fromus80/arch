3d3902fec8be5a9f4c2f4ecfbb1945d2235f5d22
Fromus80 <48888188+Fromus80@users.noreply.github.com>
2020-12-16 13:01:03 +0300

    Update arch4.sh

--- arch4.sh
+++ arch4.sh
@@ -10,7 +10,7 @@ timedatectl
 timedatectl set-local-rtc 1 --adjust-system-clock
 timedatectl
 
-sudo pacman -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader nvidia --noconfirm
+sudo pacman -S nvidia nvidia-settings opencl-nvidia lib32-opencl-nvidia lib32-nvidia-utils --noconfirm
 
 
 echo 'Ставим шрифты'
